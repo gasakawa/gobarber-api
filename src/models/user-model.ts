4b9bd3387d4e1fdf5b4fbef5def386b5fbfc4c16
Gabriel Asakawa <gabriel.asakawa@gmail.com>
2020-06-24 15:51:14 -0300

    chore: configure migration for users

---
+++ src/models/user-model.ts
@@ -0,0 +1,15 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('users')
+class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  provider: string;
+
+  @Column('timestamp with time zone')
+  date: Date;
+}
+
+export default User;
